b93912972a0fd0edc0ef9a5a67078090c6b7df26
windev0 <kossiwinnerawouno@gmail.com>
2025-05-28 19:33:47 +0000

    update

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "../.env" });
+dotenv.config();
 
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/shortener";
